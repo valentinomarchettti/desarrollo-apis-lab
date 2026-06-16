48cc9d0f5e61f2df9fc29eb7c3431e6d10c63631
Paula Rojas Frias <paularojasfrias5@gmail.com>
2026-06-15 23:52:24 -0300

    configuraciones iniciales

---
+++ api/tests/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
+from api.models import Repositorio
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def user(db):
+    # Creamos un usuario que es superusuario para saltar permisos de DRF
+    return User.objects.create_superuser(username="admin", password="password", email="admin@test.com")
+
+@pytest.fixture
+def authenticated_client(api_client, user):
+    api_client.force_authenticate(user=user)
+    return api_client
+
+@pytest.fixture
+def repo_base(db):
+    # Creamos un repositorio necesario para relacionar el Pull Request
+    return Repositorio.objects.create(
+        github_owner="usuario",
+        github_repo="repo"
+    )
